4457fb6ffe08df05683ae3c62fbab27d412f76cf
Micah Lloyd <flightx31@gmail.com>
2023-06-28 10:46:32 -0600

    initial commit

---
+++ exceptionUtil.go
@@ -0,0 +1,39 @@
+package exception
+
+type Logger interface {
+	Panic(args ...interface{})
+	Error(args ...interface{})
+	Warn(args ...interface{})
+	Info(args ...interface{})
+	Debug(args ...interface{})
+	Trace(args ...interface{})
+	Print(args ...interface{})
+}
+
+var log Logger
+
+func SetLogger(l Logger) {
+	log = l
+}
+
+func PanicOnError(e error) {
+	if e != nil {
+		log.Panic(e)
+		panic(e)
+	}
+}
+
+func LogError(e error) {
+	if e != nil {
+		log.Error(e, "")
+	}
+}
+
+func PanicOnErrorFunc(f func() error) func() {
+	return func() {
+		err := f()
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/flightx31/exception
+
+go 1.19
